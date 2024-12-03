28e68a14f60c239f2a5b3b93cc53c427a6008b3e
Bernardasko <115994039+Bernardasko@users.noreply.github.com>
2024-12-03 13:48:33 +0200

    add mainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 import './App.css'
+import Main from './components/Main.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
    <div>
