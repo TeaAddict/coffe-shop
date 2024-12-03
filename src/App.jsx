6655369b46be7ef493f6809a7bbe109231011ebe
Deividas Morkunas <deividasmor7@gmail.com>
2024-12-03 14:44:03 +0300

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
