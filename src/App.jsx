bd6d6231a999b9cdfa15001f4225bdcd63532e55
Darius <juchevicd@gmail.com>
2024-12-03 13:35:26 +0200

    instal tailwind and add new tags in header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +11,8 @@ function App() {
     <>
    <div>
     <Header/>
-    <Main/>
-    <Footer/>
+    {/* <Main/> */}
+    {/* <Footer/> */}
    </div>
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <header className=" flex justify-between items-center">
+        <section className=" flex gap-1">
+          <h1 className="text-[]">IMAJI</h1>
+          <h1>Coffee.</h1>
+        </section>
+        <nav className=" flex gap-2 ">
+          <button>Home</button>
+          <button>Story</button>
+          <button>Menu</button>
+          <button>Space</button>
+          <button>Community</button>
+          <button>News</button>
+          <section className=" flex gap-2 items-center">
+            <section className="  "> 
+            <button className=" border border-[#9d7d5c] hover:border-[#9d7d5c] p-3 ">Order</button>
+            </section>
+            <section>
+            <button className=" border border-[#9d7d5c] p-3 hover:border-[#9d7d5c]">Sing in</button>
+            </section>
+          </section>
+        </nav>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
