e47fef37d864399a54a115a201a47a7dedb1d2b1
Darius <juchevicd@gmail.com>
2024-12-03 13:59:39 +0200

    end header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,24 +3,25 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <header className=" flex justify-between items-center">
+      <header className=" flex justify-between items-center m-3 font-medium ">
         <section className=" flex gap-1">
-          <h1 className="text-[]">IMAJI</h1>
-          <h1>Coffee.</h1>
+          <h1 >IMAJI</h1>
+          <h1 className="text-[#9d7d5c] font-bold font-xl">Coffee.</h1>
         </section>
         <nav className=" flex gap-2 ">
-          <button>Home</button>
-          <button>Story</button>
-          <button>Menu</button>
-          <button>Space</button>
-          <button>Community</button>
-          <button>News</button>
+          <button className=" font-medium">Home</button>
+          <button className=" font-medium" >Story</button>
+          <button className=" font-medium" >Story</button>
+          <button className=" font-medium">Menu</button>
+          <button className=" font-medium">Space</button>
+          <button className=" font-medium">Community</button>
+          <button className=" font-medium">News</button>
           <section className=" flex gap-2 items-center">
             <section className="  "> 
-            <button className=" border border-[#9d7d5c] hover:border-[#9d7d5c] p-3 ">Order</button>
+            <button className=" border border-[#9d7d5c] hover:bg-[#9d7d5c] p-3 font-medium">Order</button>
             </section>
             <section>
-            <button className=" border border-[#9d7d5c] p-3 hover:border-[#9d7d5c]">Sing in</button>
+            <button className=" border border-[#9d7d5c] p-3 hover:bg-[#9d7d5c] font-medium">Sing in</button>
             </section>
           </section>
         </nav>
